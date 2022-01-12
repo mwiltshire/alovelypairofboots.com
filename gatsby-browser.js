ea62ea142e87bae1de9b9880b41a1972ca6ddf53
Mark Wiltshire <mark.wiltshire88@gmail.com>
2022-01-12 19:48:16 +0100

    import fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,8 @@
 import smoothscroll from 'smoothscroll-polyfill';
 
-import '@fontsource/amatic-sc';
-import '@fontsource/raleway';
+import '@fontsource/amatic-sc/400.css';
+import '@fontsource/raleway/400.css';
+import '@fontsource/raleway/700.css';
 
 export const onClientEntry = () => {
   smoothscroll.polyfill();
