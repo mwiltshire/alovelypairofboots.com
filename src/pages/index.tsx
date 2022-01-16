69ad14fc54bff02dfe241f28051ac684b54bb24f
Mark Wiltshire <mark.wiltshire88@gmail.com>
2022-01-16 17:28:17 +0100

    add toast/flower separators

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Toaster } from 'react-hot-toast';
 import { Layout } from '../components/Layout';
 import { Container } from '../components/Container';
 import { Home } from '../sections/Home';
@@ -8,17 +9,49 @@ import { Info } from '../sections/Info';
 import { Accommodation } from '../sections/Accommodation';
 import { Registry } from '../sections/Registry';
 import { Rsvp } from '../sections/Rsvp';
+import { Flower3 } from '../components/Flower3';
+import { Flower4 } from '../components/Flower4';
 
 function Index() {
   return (
     <Layout>
+      <Toaster
+        position="bottom-left"
+        toastOptions={{
+          duration: 4000,
+          success: {
+            iconTheme: { primary: 'var(--green)', secondary: 'var(--white)' }
+          },
+          error: {
+            iconTheme: { primary: 'var(--red)', secondary: 'var(--white)' }
+          },
+          style: {
+            padding: '1rem'
+          }
+        }}
+      />
       <Container>
         <Home />
         <Venue />
+        <div className="h-16 md:hidden">
+          <Flower3 />
+        </div>
         <Timetable />
+        <div className="h-16 md:hidden">
+          <Flower4 />
+        </div>
         <Info />
+        <div className="h-16 md:hidden">
+          <Flower3 />
+        </div>
         <Accommodation />
+        <div className="h-16 md:hidden">
+          <Flower4 />
+        </div>
         <Registry />
+        <div className="h-16 md:hidden">
+          <Flower3 />
+        </div>
         <Rsvp />
       </Container>
     </Layout>
