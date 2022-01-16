c493c9e40c6278611d49c9669d5570bb141992d1
Mark Wiltshire <mark.wiltshire88@gmail.com>
2022-01-16 17:29:02 +0100

    flower svg styles

--- src/components/Flower4.tsx
+++ src/components/Flower4.tsx
@@ -3,8 +3,7 @@ import * as React from 'react';
 export function Flower4() {
   return (
     <svg
-      width="100"
-      height="100"
+      className="h-full w-full"
       viewBox="0 0 100 100"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
