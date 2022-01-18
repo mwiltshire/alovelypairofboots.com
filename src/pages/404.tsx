df4202f7bb2f4d051547c7fe2dcb98e62f307445
Mark Wiltshire <mark.wiltshire88@gmail.com>
2022-01-18 22:38:55 +0100

    add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import GatsbyLink from 'gatsby-link';
+
+export default function NotFound() {
+  return (
+    <main className="flex items-center justify-center flex-col w-screen h-screen">
+      <h1 className="text-9xl uppercase font-bold">Nein</h1>
+      <p className="text-center text-lg">
+        The site's back{' '}
+        <GatsbyLink className="block underline underline-offset-4" to="/">
+          <span
+            role="img"
+            aria-label="Backhand index finger pointing left emoji"
+          >
+            👈
+          </span>{' '}
+          thattaway
+        </GatsbyLink>
+      </p>
+    </main>
+  );
+}
