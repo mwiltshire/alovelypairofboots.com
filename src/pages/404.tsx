3c21b2dd64422a01445d2777ca9ce7e675a24802
Mark Wiltshire <mark.wiltshire88@gmail.com>
2022-01-18 22:40:29 +0100

    eslint...

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
     <main className="flex items-center justify-center flex-col w-screen h-screen">
       <h1 className="text-9xl uppercase font-bold">Nein</h1>
       <p className="text-center text-lg">
-        The site's back{' '}
+        The site&rsquo;s back{' '}
         <GatsbyLink className="block underline underline-offset-4" to="/">
           <span
             role="img"
