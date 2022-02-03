2a3d8c062c3c38280c9d768a1dd580fa38cfcd17
Mark Wiltshire <mark.wiltshire88@gmail.com>
2022-02-03 22:39:53 +0100

    update accommodation

--- src/sections/Accommodation.tsx
+++ src/sections/Accommodation.tsx
@@ -11,24 +11,34 @@ export function Accommodation() {
           <Heading level={2}>Accommodation</Heading>
           <p>
             We have a very limited number of rooms in Bore Place House and the
-            Old Stables. Depending on the room, the rate will be around £60-£100
+            Old Stables. Depending on the room, the rate will be around £60-£120
             per night. Please note the bathrooms are shared by about three
             rooms. A self-service, continental breakfast will be provided in the
             shared kitchens of each house, but feel free to bring anything else
-            you may like.
+            you may like. If you are interested in a room let us know in the
+            RSVP form below and we shall do our best to accommodate you.
           </p>
           <p>
-            Bore Place also has two additional cottages, a shepherds hut, two
-            luxury yurts or the option to bring your own tent for a small pitch
-            fee (rates to be confirmed).
+            Camping is allowed on site for a small pitch fee. If you plan to do
+            this let us know in the RSVP form below so the venue can prepare the
+            site.
           </p>
           <p>
-            If you are interested in these options let us know in the RSVP form
-            below and we shall do our best to accommodate you.
+            Bore Place Farm also has two shepards huts and two yurts which you
+            can book{' '}
+            <a
+              className="underline decoration-green decoration-3 underline-offset-1"
+              href="https://www.freerangeglamping.co.uk/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              here
+            </a>
+            .
           </p>
           <p>
-            There are also some Airbnb options nearby and Sevenoaks is only a 20
-            minute drive from the venue.
+            There are also some Airbnb options in the area and Sevenoaks is only
+            a 20 minute drive from the venue.
           </p>
         </div>
       </SectionCard>
